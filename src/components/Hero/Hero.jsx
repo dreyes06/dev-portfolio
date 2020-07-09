22fff930f7f1a7648337e00ce34f3175094bdd9b
Daniel <djreyes042497@gmail.com>
2020-07-09 14:10:29 -0500

    rough draft

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,15 +26,15 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Daniel Reyes (DJ)'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || 'A Full-Stack Developer.'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
-              {cta || 'Know more'}
+              {cta || 'More Below'}
             </a>
           </p>
         </Fade>
--- src/mock/data.js
+++ src/mock/data.js
@@ -17,10 +17,12 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'profile-photo.jpg',
+  paragraphOne:
+    'As told above, my name is Daniel Reyes, but I prefer to go by DJ! I am a fun loving goofball that has a passion for music, soccer, fashion, and of course code. The challenges that come with coding are what have intrigued me and are what I have found are most rewarding when solved.',
+  paragraphTwo:
+    'After taking the advice of a close friend, I decided to pursue a career in web development by attending a 13 week bootcamp at Dev Mountain. In the begining of the course I felt so behind the level of experience that I would eventually come to, but after many long nights and games of ping-pong (yes, I know, ping-pong) I became a full-stack developer with real experience in an immersive and remote setting.',
+  paragraphThree: 'Below are projects from my time at Dev Mountain in chronological order. Enjoy.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +30,33 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'tagged.png',
+    title: 'Tagged (No Database)',
+    info:
+      'Tagged is a static blog site built with no database. It was my first project built with React and Express.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/dreyes06/NodeDB', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'dr-photo.png',
+    title: 'DR Photography',
+    info:
+      'DR Photography was my first Full-Stack website (PostgreSQL, Express, React, and Node.js). This project incorporated Stripe, a third party payment technology, that was user friendly to incorporate.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'http://142.93.67.119:6060/',
+    repo: 'https://github.com/dreyes06/DR-Photography', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'pawwaround.png',
+    title: 'PawwAround',
+    info:
+      'PawwAround was a collaborative Full-Stack dog meet up site built with two of my classmates from Dev Mountain. My role in this project was to manage the backend and RESTful API calls, whilst helping where I could with the Front-end of the project.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'http://157.245.169.87:3001/',
+    repo: 'https://github.com/project-cactus-rango/Pawwaround', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +64,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'djreyes042497@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +73,27 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/explore',
     },
     {
       id: uuidv1(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/dreyes06',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/daniel-reyes-jr/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/dreyes06',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
